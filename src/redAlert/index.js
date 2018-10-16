43f60bbc19cc6803441d761b388b238cd17f3c00
Bartłomiej Rasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2018-10-16 19:04:16 +0200

    Beautify code

--- src/redAlert/index.js
+++ src/redAlert/index.js
@@ -29,53 +29,11 @@ const startRedAlert = io => {
         socket.emit("reports", reports)
       })
 
-      socket.on("newReport", report => {
-        let newReport = new Report({
-          userID: report.userID,
-          latitude: report.latitude,
-          longitude: report.longitude,
-          description: report.description,
-          category: report.category,
-          confirmations: report.confirmations,
-          extra: report.extra
-        })
+      socket.on("newReport", report => onNewReport(io, report))
 
-        newReport.save((err, report) => {
-          if (err) console.error(err)
-          Report.find({ fixed: false }, (err, reports) => {
-            if (err) console.error(err)
-            io.emit("reports", reports)
-          })
-        })
-      })
+      socket.on("fixReport", reportID => onFixReport(io, reportID))
 
-      socket.on("fixReport", reportID => {
-        Report.findOneAndUpdate(
-          { _id: reportID },
-          { fixed: true },
-          (err, result) => {
-            if (err) console.error(err)
-            Report.find({ fixed: false }, (err, reports) => {
-              if (err) console.error(err)
-              io.emit("reports", reports)
-            })
-          }
-        )
-      })
-
-      socket.on("confirmReport", reportID => {
-        Report.findOneAndUpdate(
-          { _id: reportID },
-          { $inc: { confirmations: 1 } },
-          (err, result) => {
-            if (err) console.error(err)
-            Report.find({ fixed: false }, (err, reports) => {
-              if (err) console.error(err)
-              io.emit("reports", reports)
-            })
-          }
-        )
-      })
+      socket.on("confirmReport", reportID => onConfirmReport(io, reportID))
     }
 
     socket.on("disconnect", () => {})
@@ -86,7 +44,51 @@ const startRedAlert = io => {
   })
 }
 
-function checkAuthToken(token, callback) {
+const onNewReport = (io, report) => {
+  let newReport = new Report({
+    userID: report.userID,
+    latitude: report.latitude,
+    longitude: report.longitude,
+    description: report.description,
+    category: report.category,
+    confirmations: report.confirmations,
+    extra: report.extra
+  })
+
+  newReport.save((err, report) => {
+    if (err) console.error(err)
+    Report.find({ fixed: false }, (err, reports) => {
+      if (err) console.error(err)
+      io.emit("reports", reports)
+    })
+  })
+}
+
+const onFixReport = (io, reportID) => {
+  Report.findOneAndUpdate({ _id: reportID }, { fixed: true }, (err, result) => {
+    if (err) console.error(err)
+    Report.find({ fixed: false }, (err, reports) => {
+      if (err) console.error(err)
+      io.emit("reports", reports)
+    })
+  })
+}
+
+const onConfirmReport = (io, reportID) => {
+  Report.findOneAndUpdate(
+    { _id: reportID },
+    { $inc: { confirmations: 1 } },
+    (err, result) => {
+      if (err) console.error(err)
+      Report.find({ fixed: false }, (err, reports) => {
+        if (err) console.error(err)
+        io.emit("reports", reports)
+      })
+    }
+  )
+}
+
+const checkAuthToken = (token, callback) => {
   tokenSockets(token, user => {
     if (user) {
       callback(null, user)
