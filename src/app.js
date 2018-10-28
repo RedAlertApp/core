ace7069da1613e3119e5ee3f41882d17005c71b1
Bartłomiej Rasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2018-10-28 20:33:03 +0100

    Add GraphQL first schema

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
 import "./logrocketSetup"
 
 import http from "http"
+import socketIO from "socket.io"
+import graphqlHTTP from "express-graphql"
+
 import { env, mongo, port, ip, apiRoot } from "./config"
 import mongoose from "./services/mongoose"
 import express from "./services/express"
 import api from "./api"
-import socketIO from "socket.io"
 import startRedAlert from "./redAlert"
+import { schema, root } from "./graphql"
 
 const app = express(apiRoot, api)
 const httpServer = http.createServer(app)
 const io = socketIO(httpServer)
 
 startRedAlert(io)
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+)
 
 mongoose.connect(
   mongo.uri,
